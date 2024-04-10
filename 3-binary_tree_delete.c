0ee28601ca970bad28f1ea8d1570e6d7ff1fa661
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-11 07:16:42 +0800

    Exo 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Binary Tree Delete
+ * @tree: The parent node
+ * Description: Delete a tree completely
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+    binary_tree_t *lc, *rc;
+    if (tree != NULL)
+    {
+        lc = tree->left;
+        rc = tree->right;
+        tree->left = NULL;
+        tree->right = NULL;
+        tree->parent = NULL;
+        free(tree);
+        binary_tree_delete(lc);
+        binary_tree_delete(rc);
+    }
+}

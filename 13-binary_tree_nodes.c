7e46cc6b49831b86f69ae86b9443024fae4321d0
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-11 19:36:55 +0800

    Exo 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - Binary Tree Nodes
+ * @tree: The tree to traverse
+ * Description: Counting the number of nodes with atleat one child
+ * Return: The number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->left != NULL || tree->right != NULL)
+return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+return (0);
+}

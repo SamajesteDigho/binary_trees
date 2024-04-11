717ec083bde609393b69d8b9d28212a8ca891b5f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-12 05:04:46 +0800

    Exo 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+/**
+ * get_node_depth - Get Node Depth
+ * @tree: Node to consider
+ * Description: Get the depth of the node
+ * Return: Tree depth
+ */
+int get_node_depth(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (1 + get_node_depth(tree->parent));
+}
+
+/**
+ * binary_trees_ancestor - Binary Trees Ancestor
+ * @first: First node
+ * @second: Second node
+ * Description:
+ * Return: Ancestor tree
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
+{
+int fd, sd, rollback;
+binary_tree_t *node;
+fd = get_node_depth(first);
+sd = get_node_depth(second);
+
+if (fd >= sd)
+{
+node = first->parent;
+rollback = fd - sd;
+}
+else
+{
+node = second->parent;
+rollback = sd - fd;
+}
+while (rollback > 0)
+{
+if (node == first || node == second)
+return (node);
+if (node != NULL)
+node = node->parent;
+rollback -= 1;
+}
+return (node);
+}

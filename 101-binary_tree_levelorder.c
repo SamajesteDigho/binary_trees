65ca57953418951ae3e3fff27aadb3d2431ec7fc
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-12 06:41:04 +0800

    Exo 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,18 +1,49 @@
 #include "binary_trees.h"
-void level_action(const binary_tree_t *tree, void (*func)(int), int start, int end)
+/**
+ * get_depth - Get Depth
+ * @tree: The tree to traverse
+ * Description: Get the depth of a given tree
+ * Return: The depth
+ */
+int get_depth(const binary_tree_t *tree)
 {
-    if (tree != NULL && func != NULL && start >= 0 && end >= 0)
-    {
-        if (start == end)
-        {
-            func(tree->n);
-        }
-        else
-        {
-            level_action(tree->left, func, start + 1, end);
-            level_action(tree->right, func, start + 1, end);
-        }
-    }
+int hl, hr;
+if (tree == NULL)
+return (0);
+hl = get_depth(tree->left);
+hr = get_depth(tree->right);
+if (hl >= hr)
+return (1 + hl);
+else
+return (1 + hr);
+}
+
+/**
+ * binary_tree_height - Binary Tree Hieght
+ * @tree: The tree to traverse
+ * Description: Getting the hieght of the tree
+ * Return: The height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (get_depth(tree) - 1);
+}
+
+void level_action(const binary_tree_t *tree, void (*func)(int),
+int start, int end)
+{
+if (tree != NULL && func != NULL && start >= 0 && end >= 0)
+{
+if (start == end)
+func(tree->n);
+else
+{
+level_action(tree->left, func, start + 1, end);
+level_action(tree->right, func, start + 1, end);
+}
+}
 }
 
 /**
@@ -23,12 +54,11 @@ void level_action(const binary_tree_t *tree, void (*func)(int), int start, int e
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    int i;
-    if (tree != NULL && func != NULL)
-    {
-        for (i = 0; i < 5; i++)
-        {
-            level_action(tree, func, 0, i);
-        }
-    }
+int i, depth;
+if (tree != NULL && func != NULL)
+{
+depth = get_depth(tree);
+for (i = 0; i < depth; i++)
+level_action(tree, func, 0, i);
+}
 }

fff3ebbd5978b14738b51309b089aeacbe73cece
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-12 03:52:04 +0800

    Exo 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - Binary Tree Sibling
+ * @node: Node concerned
+ * Description: Find sibling of a node
+ * Return: 1 or 0
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL)
+return (NULL);
+if (node->parent->left == node)
+return (node->parent->right);
+else
+return (node->parent->left);
+}

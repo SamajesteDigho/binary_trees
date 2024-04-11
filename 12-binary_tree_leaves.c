5683bf99fa89f8c979d9033c6322b03a5589363f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-11 13:49:57 +0800

    Exo 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - Binary Tree Leaves
+ * @tree: The tree to traverse
+ * Description: Counting the number of leaves
+ * Return: The number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}

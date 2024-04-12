0b64b9afbe68e2f87b1477691a406f80e793bf58
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-13 02:16:48 +0800

    Exo 104

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_right - Binary Trees Rotate Right
+ * @tree: Tree concerned
+ * Description: Rotate tree to the right
+ * Return: Pointer to rotated tree
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+binary_tree_t *x, *y;
+if (tree == NULL)
+return (NULL);
+if (tree->left == NULL)
+return (tree);
+y = tree;
+x = tree->left;
+y->left = x->right;
+x->right = y;
+x->parent = y->parent;
+y->parent = x;
+return (x);
+}
